ad8a928fbcd6aa0e8e0250fe49280ed5bceb319f
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2023-10-01 12:21:05 +0200

    Build docker image

--- flake.nix
+++ flake.nix
@@ -8,7 +8,8 @@
 
   outputs = { self, nixpkgs, naersk }:
     let forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
-    in {
+    in
+    {
       packages = forAllSystems (system:
         let
           pkgs = import nixpkgs { inherit system; };
@@ -18,9 +19,31 @@
           latexfogel = naersk'.buildPackage
             {
               root = ./.;
-              nativeBuildInputs = [ ];
+              nativeBuildInputs = with pkgs; [ pkg-config openssl graphite2 icu freetype fontconfig ];
             };
           default = latexfogel;
+          docker = pkgs.dockerTools.buildLayeredImage {
+            name = "kitmatheinfo/latexfogel";
+            tag = latexfogel.version;
+
+            contents = [
+              latexfogel
+              pkgs.cacert                 # or reqwest is very unhappy
+              pkgs.fontconfig             # or tectonic fails
+              pkgs.bash                   # or magick can not spawn `gs`
+              pkgs.imagemagick            # to convert, imagemagick_light has no adapter
+              pkgs.ghostscript_headless   # to convert
+            ];
+
+            config = {
+              Cmd = [ "/bin/latexfogel" ];
+              WorkingDir = "/";
+              Env = [
+                "FONTCONFIG_FILE=${pkgs.fontconfig.out}/etc/fonts/fonts.conf"
+                "FONTCONFIG_PATH=${pkgs.fontconfig.out}/etc/fonts/"
+              ];
+            };
+          };
         }
       );
     };
