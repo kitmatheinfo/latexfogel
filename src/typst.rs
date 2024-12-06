12a0395200bd6a15ec3f55653bc7b732b6668c8e
I-Al-Istannen <i-al-istannen@users.noreply.github.com>
2024-12-07 00:35:50 +0100

    Revert "Make typst png background transparent"
    This reverts commit e66e80f6489691a6d6e79bd554959823066eb02a.
    
    It was a nice idea, but it turns out that white-on-white isn't much more
    readable sadly.

--- src/typst.rs
+++ src/typst.rs
@@ -177,6 +177,7 @@ impl World for DummyWorld {
 pub fn render_to_png(typst: String) -> anyhow::Result<Vec<u8>> {
     let typst = [
         "#set page(width: 11.5cm, height: auto, margin: (x: 1mm, y: 2mm))",
+        "#set page(fill: rgb(\"#313338\"))", // Discord background color
         "#set text(white)",
         &typst,
     ]
@@ -195,7 +196,7 @@ pub fn render_to_png(typst: String) -> anyhow::Result<Vec<u8>> {
         anyhow!("Failed to compile typst code:\n\n{errs}")
     })?;
 
-    // If no background color is specified, it is transparent by default.
+    // Color doesn't matter, it is already set by the document itself
     let png = typst_render::render_merged(&document, 4.0, Abs::zero(), None).encode_png()?;
 
     Ok(png)
